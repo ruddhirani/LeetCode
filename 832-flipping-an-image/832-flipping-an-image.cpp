1b347b836eb824167b92b1b2671d76e00231ea0c
ruddhirani <90724307+ruddhirani@users.noreply.github.com>
2022-06-30 12:03:17 +0530

    Time: 3 ms (94.34%), Space: 8.7 MB (93.58%) - LeetHub

--- 832-flipping-an-image/832-flipping-an-image.cpp
+++ 832-flipping-an-image/832-flipping-an-image.cpp
@@ -4,19 +4,19 @@ public:
         for(int i=0;i<image.size();i++){
             int st=0;
             int end=image[i].size()-1;
-            while(st<end){
-                int temp=(image[i][end]);
-                image[i][end]=(image[i][st]);
+            while(st<=end){
+                int temp=(image[i][end]^1);
+                image[i][end]=(image[i][st]^1);
                 image[i][st]=temp;
                 st++;
                 end--;
             }
         }
-        for(int i=0;i<image.size();i++){
-            for(int j=0;j<image[i].size();j++){
-                image[i][j]^=1; 
-            }
-        }
+        // for(int i=0;i<image.size();i++){
+        //     for(int j=0;j<image[i].size();j++){
+        //         image[i][j]^=1; 
+        //     }
+        // }
         return image;
     }
 };

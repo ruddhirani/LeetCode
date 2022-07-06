78e55e79a1603f7b8289eb5d7a2cb9f7832c990d
ruddhirani <90724307+ruddhirani@users.noreply.github.com>
2022-07-06 11:37:16 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (18.24%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int fib(int n) {
+       int *arr=new int[n+1];
+        if(n==0)
+        {
+            return 0;
+        }
+        else
+            if(n==1)
+        {
+            return 1; 
+        }
+        arr[0]=0;
+        arr[1]=1;
+        for(int i=2;i<=n;i++){
+            arr[i]=arr[i-1]+arr[i-2];
+        }
+        int output=arr[n];
+        return output;
+    }
+};

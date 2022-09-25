57b952e12b6d49a34f0ab46f4b2954cb07cd5eb0
ruddhirani <90724307+ruddhirani@users.noreply.github.com>
2022-09-25 13:02:07 +0530

    Time: 6 ms (10.56%), Space: 6.5 MB (70.85%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> res(numRows);
-        for(int i=0;i<numRows;i++){
-            res[i].resize(i+1);
-            res[i][0]=res[i][i]=1;
-            for(int j=1;j<i;j++){
-                res[i][j]=res[i-1][j-1]+res[i-1][j];
+        //created a 2d vector intiating size of number of rows
+        vector<vector<int>> pas(numRows);
+        int i,j;
+        for(i=0;i<numRows;i++){
+            //making columns of size n+1
+            pas[i].resize(i+1);
+            //putting 1 at the start and end of the row
+            pas[i][0]=pas[i][i]=1;
+            
+            //filling the rest of the pattern with adding the elements of previous row
+            for(j=1;j<i;j++){
+                pas[i][j]=pas[i-1][j-1]+pas[i-1][j];
             }
         }
-        return res;
+        return pas;
     }
 };

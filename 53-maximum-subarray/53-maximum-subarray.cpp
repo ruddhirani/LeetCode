21f516bc0aa8a42f0462464e33840f29ca0886e8
ruddhirani <90724307+ruddhirani@users.noreply.github.com>
2022-10-02 12:59:48 +0530

    Time: 136 ms (80.83%), Space: 67.7 MB (90.31%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,14 +1,17 @@
+#include<bits/stdc++.h>
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int res=nums[0],m=0;
-        for(int i=0;i<nums.size();i++){
-            m+=nums[i];
-            if(res<m)
-                res=m;
-            if(m<0)
-                m=0;
+        //initiating values
+        int sum=0;
+        int max_sum=INT_MIN;
+        for(auto it: nums){
+            sum+=it;
+            //storing max sum value
+            max_sum=max(sum,max_sum);
+            //as we need the maximum sum of the calculating sum as long as it is positive or else making it zero.
+            if(sum<0) sum=0;
         }
-        return res;
+        return max_sum;
     }
 };
